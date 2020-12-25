fc162f06f9d0749a554785aaf9399e8bfd38028c
Fncteam <fncdevteam@gmail.com>
2020-12-25 10:27:41 +0530

    Updates

---
+++ precache-manifest.be98bbf330e05c4e21743d833805e1f5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d683970c4988e507f036",
+    "url": "/FNC/static/css/main.61ad8e01.chunk.css"
+  },
+  {
+    "revision": "d683970c4988e507f036",
+    "url": "/FNC/static/js/main.fa94aa99.chunk.js"
+  },
+  {
+    "revision": "bc20d4aca627b40ecda5",
+    "url": "/FNC/static/js/runtime~main.3fdd263c.js"
+  },
+  {
+    "revision": "f253928e9643d9501e49",
+    "url": "/FNC/static/css/2.5bdd73bc.chunk.css"
+  },
+  {
+    "revision": "f253928e9643d9501e49",
+    "url": "/FNC/static/js/2.8496c660.chunk.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/FNC/static/media/logo.9f7e506d.svg"
+  },
+  {
+    "revision": "a8d9f58038c81fe8547435eaa63915b5",
+    "url": "/FNC/static/media/FNC_logo.a8d9f580.jpg"
+  },
+  {
+    "revision": "b218511fd9e5110abd585bccf1afd10b",
+    "url": "/FNC/index.html"
+  }
+];
